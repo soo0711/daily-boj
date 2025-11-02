5026f4bb4befbce24523025f4ab5257114ab1d22
soo0711 <k300hys@naver.com>
2025-11-02 20:26:42 +0900

    BOJ 24266 알고리즘의 수행 시간5

---
+++ src/boj_24266_알고리즘의_수행_시간5/Main.java
@@ -0,0 +1,15 @@
+package boj_24266_알고리즘의_수행_시간5;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        long n = Integer.parseInt(br.readLine());
+        System.out.println(n*n*n);
+        System.out.println(3);
+    }
+}
