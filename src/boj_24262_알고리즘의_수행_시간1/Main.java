b9c227f35d9de7be74865618974134831b974415
soo0711 <k300hys@naver.com>
2025-11-02 19:28:28 +0900

    BOJ 24262 알고리즘의 수행 시간1

---
+++ src/boj_24262_알고리즘의_수행_시간1/Main.java
@@ -0,0 +1,15 @@
+package boj_24262_알고리즘의_수행_시간1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        System.out.println(1);
+        System.out.println(0);
+    }
+}
