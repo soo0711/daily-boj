4da4287e1da414c0c0edcb160cdfacae1127d905
soo0711 <k300hys@naver.com>
2025-12-08 23:24:50 +0900

    BOJ 2750 수 정렬하기

---
+++ src/boj_2750_수_정렬하기/Main.java
@@ -0,0 +1,27 @@
+package boj_2750_수_정렬하기;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        int[] num = new int[n];
+
+        // 정렬
+        for (int i = 0; i < n; i++){
+            num[i] = Integer.parseInt(br.readLine());
+        }
+
+        Arrays.sort(num);
+
+        for (int i = 0; i < n; i++) {
+            System.out.println(num[i]);
+        }
+    }
+}
