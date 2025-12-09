7f7927e8319da8579935b5afe3a138de10e0b6d5
soo0711 <k300hys@naver.com>
2025-12-09 15:55:24 +0900

    BOJ 25305 커트라인

---
+++ src/boj_25305_커트라인/Main.java
@@ -0,0 +1,27 @@
+package boj_25305_커트라인;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int cut = Integer.parseInt(st.nextToken());
+        Integer[] num = new Integer[n];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            num[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(num);
+        System.out.println(num[n - cut]);
+    }
+}
