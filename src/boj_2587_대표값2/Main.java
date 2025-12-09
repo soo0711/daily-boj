a7c0c65b9897625ce1051208bd484deec777278a
soo0711 <k300hys@naver.com>
2025-12-09 15:43:32 +0900

    BOJ 2587 대표값2

---
+++ src/boj_2587_대표값2/Main.java
@@ -0,0 +1,26 @@
+package boj_2587_대표값2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int[] num = new int[5];
+        int sum = 0;
+
+        for (int i = 0; i < num.length; i++) {
+            num[i] = Integer.parseInt(br.readLine());
+            sum += num[i];
+        }
+
+        Arrays.sort(num);
+        System.out.println(sum / num.length);
+        System.out.println(num[2]);
+
+    }
+}
