0baba5d51ce4d36a37fdfcc58dfef83c1796ec39
soo0711 <k300hys@naver.com>
2025-12-29 16:21:50 +0900

    BOJ 13909 창문 닫기

---
+++ src/boj_13909_창문_닫기/Main.java
@@ -0,0 +1,18 @@
+package boj_13909_창문_닫기;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        long n = Long.parseLong(br.readLine());
+
+        System.out.println((long) Math.sqrt(n));
+
+    }
+}
