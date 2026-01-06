50153f23b0090b1509e2cf2679f21dbbf7d345e6
soo0711 <k300hys@naver.com>
2026-01-06 19:27:06 +0900

    BOJ 15439 베라의 패션

---
+++ src/boj_15439_베라의_패션/Main.java
@@ -0,0 +1,16 @@
+package boj_15439_베라의_패션;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        System.out.println(n * (n - 1));
+    }
+}
