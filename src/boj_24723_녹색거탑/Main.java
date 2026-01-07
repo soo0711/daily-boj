cb644aa4858e0fcc18f7a40b345c94c11141682a
soo0711 <k300hys@naver.com>
2026-01-07 22:47:39 +0900

    BOJ 24723 녹색거탑

---
+++ src/boj_24723_녹색거탑/Main.java
@@ -0,0 +1,15 @@
+package boj_24723_녹색거탑;
+
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        System.out.println(1 << n);
+    }
+}
