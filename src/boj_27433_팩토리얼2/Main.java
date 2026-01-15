2fdf435394d38a3b87467c2d3553bf3a071bb364
soo0711 <k300hys@naver.com>
2026-01-15 21:31:01 +0900

    BOJ 27433 팩토리얼2

---
+++ src/boj_27433_팩토리얼2/Main.java
@@ -0,0 +1,24 @@
+package boj_27433_팩토리얼2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        System.out.println(factorial(n));
+    }
+
+    public static long factorial (int num) {
+        if (num <= 1) {
+            return 1;
+        }
+
+        return num * factorial(num - 1);
+    }
+}
