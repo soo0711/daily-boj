6afbe222cdc3a269c09c16efaa2cfcbd72af3e4f
soo0711 <k300hys@naver.com>
2026-01-16 19:41:45 +0900

    BOJ 25501 재귀의 귀재

---
+++ src/boj_25501_재귀의_귀재/Main.java
@@ -0,0 +1,34 @@
+package boj_25501_재귀의_귀재;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    static int cnt = 0;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int n = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < n; i++) {
+            sb.append(isPalindrome(br.readLine())).append(" ").append(cnt).append("\n");
+        }
+        System.out.println(sb);
+
+    }
+
+    public static int recursion(String s, int l, int r){
+        cnt++;
+        if(l >= r) return 1;
+        else if(s.charAt(l) != s.charAt(r)) return 0;
+        else return recursion(s, l+1, r-1);
+    }
+
+    public static int isPalindrome(String s){
+        cnt = 0;
+        return recursion(s, 0, s.length()-1);
+    }
+}
