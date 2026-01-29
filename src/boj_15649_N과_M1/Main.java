6062ddc76baa541bfb04ed968d4d0473a46de08a
soo0711 <k300hys@naver.com>
2026-01-29 23:54:02 +0900

    BOJ 15649 N과 M(1)

---
+++ src/boj_15649_N과_M1/Main.java
@@ -0,0 +1,51 @@
+package boj_15649_N과_M1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static int n, m;
+
+    static boolean[] visited;
+
+    static int[] arr;
+
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        visited = new boolean[n + 1];
+        arr = new int[m];
+        dfs(0);
+
+        System.out.println(sb);
+    }
+
+    public static void dfs(int depth) {
+        if (depth == m) {
+            for (int i = 0; i < m; i++) {
+                sb.append(arr[i]).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for (int i = 1; i <= n; i++) {
+            if (!visited[i]) {
+                visited[i] = true;
+                arr[depth] = i;
+                dfs(depth + 1);
+                visited[i] = false;
+            }
+        }
+    }
+}
